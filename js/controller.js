b993dee897328229aa2ee0f3b5e6c6bc5ea7cdcc
rubensdev <rubensdeveloper@gmail.com>
2014-01-11 22:51:34 +0000

    Modified design and about page added

--- js/controller.js
+++ js/controller.js
@@ -3,7 +3,6 @@ var postsApp = angular.module('blogApp', ['ngSanitize']);
 postsApp.controller('PostsListCtrl', function($scope){
 	/*$scope.posts = [{% for post in site.posts %} { 'title' : '{{ post.title }}', 'url' : '{{ post.url }}',
 	'day_month' : '{{ post.date | date: "%d %b" }}', 'year' : '{{ post.date | date: "%Y" }}',
-	'content' : '{{ post.content | strip_html | strip_newlines | truncate : 120 }} <a href="{{post.url}}">Read More</a>'}, {% endfor %}];*/
+	'content' : '{{ post.content | strip_html | strip_newlines | truncate : 120 }} <a class="read_more" href="{{post.url}}">Read More</a>'}, {% endfor %}];*/
 	$scope.posts = postsApp.posts;
-	$scope.hello = "Hello world!";
 });
